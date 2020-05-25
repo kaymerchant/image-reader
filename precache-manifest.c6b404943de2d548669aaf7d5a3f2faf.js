1855ff52685d3c330e32430b26b25345b4a6ae8a
Kay Merchant <58919421+kaymerchant@users.noreply.github.com>
2020-05-24 23:52:12 -0400

    Updates

---
+++ precache-manifest.c6b404943de2d548669aaf7d5a3f2faf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b07a789e879c1518d0e266a4d747e282",
+    "url": "/kaymerchant/github.io/image-reader/index.html"
+  },
+  {
+    "revision": "529e5a439be8637630e3",
+    "url": "/kaymerchant/github.io/image-reader/static/css/main.15391aaf.chunk.css"
+  },
+  {
+    "revision": "27ee107848e1651b3d32",
+    "url": "/kaymerchant/github.io/image-reader/static/js/2.33bc4f8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kaymerchant/github.io/image-reader/static/js/2.33bc4f8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "529e5a439be8637630e3",
+    "url": "/kaymerchant/github.io/image-reader/static/js/main.4dc1fe59.chunk.js"
+  },
+  {
+    "revision": "d241f831a6529b24b6f7",
+    "url": "/kaymerchant/github.io/image-reader/static/js/runtime-main.89ed930c.js"
+  }
+]);
